b4d7e5355f79b2303d21f60356394b264690f5f7
rendy <rendyshu123@gmail.com>
2024-02-21 22:32:32 +0700

    firstcommit

---
+++ app/Http/Controllers/Controller1.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Controller1 extends Controller
+{
+
+    public function index(){
+
+    return view('example');
+    }
+}
